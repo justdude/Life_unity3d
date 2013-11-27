eb394c4f615e95a8837854686d32b35a349cf09a
justdude <ivan.albantov@tatemgames.com>
2013-11-27 04:34:02 +0200

    first

---
+++ Unity3d/Assets/Scripts/DNK.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+    public class DNK
+    {
+        private List<int> genealogicThree;
+        private  byte generationCount;
+        public int dnk = 0;
+
+        public DNK(int mother,int father){
+            
+        }
+
+        public int getGeneration()
+        {
+            return generationCount;
+        }
+
+        public void IncGeneration() {
+            ++generationCount;
+        }
+
+        public static void Encrypt(ref int value,int key) {
+            value = value ^ key;
+        }
+
+        public static void Decrypt(ref int value, int key)
+        {
+            Encrypt(ref value, key);
+        }
+
+
+    }
---
+++ Unity3d/Assets/Scripts/World.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class World : MonoBehaviour {
+
+    public static Settings settings;
+    public static CellGraphicController graphic;
+
+    public Settings settings_;
+    public CellGraphicController graphic_;
+
+    void Awake() {
+        settings = settings_;
+        graphic = graphic_;
+    }
+
+    public static List<Element>  GetDataFromTwoDimArray(GameObject[,] elements)
+    {
+        int n = elements.GetLength(0);
+        int m = elements.GetLength(1);
+        List<Element> elementsL = new List<Element>();
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+            {
+                elementsL.Add(elements[i, j].GetComponent<Element>());
+            }
+        return elementsL;
+    }
+
+    public static Vect2 ConvertLToArr(int i, int I, int J)
+    {
+        return new Vect2(i / I, i % J);
+    }
+
+    public static Vect2 ConvertLToArr(int i, int I)
+    {
+        return new Vect2(i / I, i % I);
+    }
+
+    public static int ConvertArrToL(int i, int j, int I)
+    {
+        return i*I + j;
+    }
+}
